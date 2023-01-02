d553b1e2226d01006da4d6e383e59e17a8271c5e
Farhan Tariq <iamfarhantariq@gmail.com>
2023-01-03 01:38:23 +0500

    default font family added

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
  * @format
  */
 
-import {AppRegistry, Text, TextInput} from 'react-native';
+import { AppRegistry, Text, TextInput } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import AppStyle from './src/assets/styles/AppStyle';
 
 AppRegistry.registerComponent(appName, () => App);
 
 if (Text.defaultProps == null) {
     Text.defaultProps = {};
     Text.defaultProps.allowFontScaling = false;
+    Text.defaultProps.style = { fontFamily: AppStyle.fontFamily.Noto.regular.family }
 }
 
 if (TextInput.defaultProps == null) {
     TextInput.defaultProps = {};
     TextInput.defaultProps.allowFontScaling = false;
+    TextInput.defaultProps.style = { fontFamily: AppStyle.fontFamily.Noto.regular.family }
 }
