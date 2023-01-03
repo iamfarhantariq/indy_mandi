42453787a04556480e537d87508fe5b164646afb
Farhan Tariq <iamfarhantariq@gmail.com>
2023-01-04 02:54:07 +0500

    changes

---
+++ src/components/Products/GeneralProduct.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GeneralProduct = () => {
+  return (
+    <View>
+      <Text>GeneralProduct</Text>
+    </View>
+  )
+}
+
+export default GeneralProduct;
+
+const styles = StyleSheet.create({})
