3f3317f9cfb4ba2083ef16e5267c0e16a3b68d5f
Farhan Tariq <iamfarhantariq@gmail.com>
2023-08-19 15:05:30 +0500

    changes

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,8 +1,7 @@
 import { Dimensions, Platform, StatusBar } from 'react-native';
 
-// const BASE_API_URL = 'https://indymandi.com/api'; // Dev maybe
-const BASE_API_URL = 'https://staging.indymandi.com/api'; // staging maybe
-const GOOGLE_WEB_CLIENT_ID = '1085172821890-m210dk82cieq062n99d890uu3bsos42p.apps.googleusercontent.com';
+const BASE_API_URL = 'BASE_URL'; // staging maybe
+const GOOGLE_WEB_CLIENT_ID = 'GOOGLE_WEB_CLIENT';
 
 const SCREEN_WIDTH = Dimensions.get('screen').width;
 const SCREEN_HEIGHT = Dimensions.get('screen').height;
@@ -14,15 +13,10 @@ const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 45 : StatusBar.currentHeight;
 
 const BOTTOM_TABS_HEIGHT = Platform.OS === 'ios' ? 100 : 82;
 
-// const _EMAIL_REG_EXP = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-
-const PUSHER_APP_ID = 859996
-const PUSHER_APP_KEY = '09dc0a50ffe300c08cb8'
-const PUSHER_APP_SECRET = '6e4cb89e9e6159ef779d'
-const PUSHER_APP_CLUSTER = 'ap2'
-
-// const RAZORPAY_KEY = 'rzp_test_d5KOvIhSHN84Ff'
-// const RAZORPAY_SECRET = 'CSzWPXeBnO0fTc2kuoxfnL3Z'
+const PUSHER_APP_ID = 'PUSHER_APP_ID'
+const PUSHER_APP_KEY = 'PUSHER_APP_KEY'
+const PUSHER_APP_SECRET = 'PUSHER_APP_SECRET'
+const PUSHER_APP_CLUSTER = 'PUSHER_APP_CLUSTER'
 
 const AppConfig = {
   baseApiURL: BASE_API_URL,
